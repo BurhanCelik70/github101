6ce299378033f8143160987abba9fc4e4064e7b7
Burhan Çelil <celikburhan56@gmail.com>
2025-05-03 01:12:57 +0300

    burhan

--- code_1.py
+++ code_1.py
@@ -1,14 +1,13 @@
 import time
-# First line of our first code
+
+# First line of your first code
 print("Hello world!")
 
 # New line is added
 print("Hey! I'm changing the file!")
 
-for i in range(5,-1,-1):
-    print("-->", i)
+for i in range(5, -1, -1):
+    print("--->", i)
     time.sleep(1)
 
 print("Rocket was launched!")
-print("hello world")
-print("hello world")
